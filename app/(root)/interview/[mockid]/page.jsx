7962e1cdf0fab5f547174d9a76e8377e4c72a7b5
Abir Banerjee <abirbanerjee793@gmail.com>
2024-09-22 19:07:50 +0530

    Data fetched from Database

--- app/(root)/interview/[mockid]/page.jsx
+++ app/(root)/interview/[mockid]/page.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+"use client";
+import { useParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import { db } from '../../../../utils/db';
+import { MockInterview } from '../../../../utils/schema';
+import { eq } from 'drizzle-orm';
 
 const page = () => {
+    const [InterviewData, setInterviewData] = useState()
+    const {mockid} = useParams();
+    useEffect(()=>{
+        console.log(mockid)
+        fetchInterviewDetails();
+    }, [])
+    
+    const fetchInterviewDetails = async() =>{
+        const data = await db.select().from(MockInterview).
+        where(eq(MockInterview.mockId, mockid));
+        setInterviewData(data[0])
+    }
   return (
     <div>Interview Q/A</div>
   )
