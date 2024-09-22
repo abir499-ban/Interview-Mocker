f671c9d784102995df8eaedfc5c7632d0a7f7cb8
Abir Banerjee <abirbanerjee793@gmail.com>
2024-09-22 20:01:08 +0530

    Web cam feature enabled(mnw)

--- app/(root)/interview/[mockid]/page.jsx
+++ app/(root)/interview/[mockid]/page.jsx
@@ -4,23 +4,45 @@ import React, { useEffect, useState } from 'react'
 import { db } from '../../../../utils/db';
 import { MockInterview } from '../../../../utils/schema';
 import { eq } from 'drizzle-orm';
+import Webcam from 'react-webcam';
+import { WebcamIcon } from 'lucide-react';
+import { Button } from '../../../../components/ui/button';
 
 const page = () => {
     const [InterviewData, setInterviewData] = useState()
-    const {mockid} = useParams();
-    useEffect(()=>{
+    const [enablewebcam, setenablewebcam] = useState(false);
+    const { mockid } = useParams();
+    useEffect(() => {
         console.log(mockid)
         fetchInterviewDetails();
     }, [])
-    
-    const fetchInterviewDetails = async() =>{
+
+    const fetchInterviewDetails = async () => {
         const data = await db.select().from(MockInterview).
-        where(eq(MockInterview.mockId, mockid));
+            where(eq(MockInterview.mockId, mockid));
         setInterviewData(data[0])
     }
-  return (
-    <div>Interview Q/A</div>
-  )
+    return (
+        <div>
+            <div className='text-2xl text-black flex items-center justify-center font-bold font-mono'>
+                Lets get started!!</div>
+            {enablewebcam ? (
+                <Webcam
+                    onUserMedia={() => { setenablewebcam(true) }}
+                    onUserMediaError={() => { setenablewebcam(false) }}
+                    style={{
+                        height: 300,
+                        width: 300
+                    }} />
+            ) : (
+                <div className='flex flex-wrap flex-col gap-2 justify-center items-center'>
+                    <WebcamIcon size={100} color="#d62e2e" strokeWidth={1.5} />
+                    <Button onClick={() => setenablewebcam(true)}>Enable Webcam and Microphone</Button>
+                </div>
+            )}
+
+        </div>
+    )
 }
 
 export default page
